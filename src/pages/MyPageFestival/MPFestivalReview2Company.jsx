954d5eff34648944751444ace01a13ae39c84fbf
이예나 <iyena@iyenas-MacBook-Pro.local>
2025-08-16 18:38:05 +0900

    리뷰1

--- src/pages/MyPageFestival/MPFestivalReview2Company.jsx
+++ src/pages/MyPageFestival/MPFestivalReview2Company.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 import Navbar from '../../components/Navbar'
 import { useParams } from 'react-router-dom'
+import fest_data from '../../assets/fest/fest_data'
+import co_data from '../../assets/company/co_data'
 
 const MPFestivalReview2Company = () => {
 
     const {festivalId, companyId} = useParams();
+    const festival = fest_data.find(f => f.festivalId === Number(festivalId));
+    const company = co_data.find(c => c.companyId === Number(companyId));
 
 
 
@@ -17,7 +21,20 @@ const MPFestivalReview2Company = () => {
                     <p>리뷰 쓰기</p>
                 </Title>
             </Fixed>
-            <p>festivalId:{festivalId}companyId:{companyId}</p>
+            <p>{festival?.festivalName}</p>
+
+            <CompanyInfo>
+                <img src={company?.image} alt="" />
+                <p>{company?.companyName}</p>
+                <p>{company?.address}</p>
+                <p>{company?.companyCategory}</p>
+            </CompanyInfo>
+            <Review>
+                <p>리뷰를 작성해주세요(200자이내)</p>
+                <input type="text" />
+                <button>등록하기</button>
+            </Review>
+
 
 
 
@@ -44,4 +61,12 @@ const Title = styled.div`
   background-color: rgb(199, 199, 199);
   font-size: 22px;
   padding: 30px 0 30px 270px;
+`;
+
+const CompanyInfo = styled.div`
+    
+`;
+
+const Review = styled.div`
+    
 `;
