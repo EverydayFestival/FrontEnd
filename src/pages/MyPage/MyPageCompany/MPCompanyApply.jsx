f36da8606b1a6810ff44fe66560b7500f2227feb
이예나 <iyena@iyenas-MacBook-Pro.local>
2025-08-22 14:10:42 +0900

    company css, modal needed

--- src/pages/MyPage/MyPageCompany/MPCompanyApply.jsx
+++ src/pages/MyPage/MyPageCompany/MPCompanyApply.jsx
@@ -39,19 +39,21 @@ const MPFestivalFavored = () => {
                 setViewAllOngoingFest(false);
                 scrollToTop();
                 }}>이전 페이지로</Back>
-                <p>진행 및 예정인 행사</p>
+                <Type>진행 및 예정인 행사</Type>
                 <FestCardList>
                   {fest_data.map((fest, index) => (
                     <FestCard key={index}>
                       <FestImage src={fest.image} alt="" />
                      <FestLeft>
                         <FestInfo>
+                            <RealInfo>
                             <FestName>{fest.festivalName}</FestName>
                             <p>{fest.location}</p>
                             <p>{fest.period}</p>
+                            </RealInfo>
                             <ApplicationBtn>지원서 보기</ApplicationBtn>
                         </FestInfo>
-                      <MoreIcon src={more_button} alt="업체더보기" />
+                      {/* <MoreIcon src={more_button} alt="업체더보기" /> */}
                      </FestLeft>
                      
                     <Status>
@@ -71,22 +73,24 @@ const MPFestivalFavored = () => {
                 setViewAllOngoingFest(false);
                 scrollToTop();
                 }}>이전 페이지로</Back>
-                <p>종료된 행사</p>
+                <Type>종료된 행사</Type>
                 <FestCardList>
                   {fest_data.map((fest, index) => (
                     <FestCard key={index}>
                       <FestImage src={fest.image} alt="" />
                      <FestLeft>
                         <FestInfo>
+                            <RealInfo>
                             <FestName>{fest.festivalName}</FestName>
                             <p>{fest.location}</p>
                             <p>{fest.period}</p>
+                            </RealInfo>
                             <ApplicationBtn>지원서 보기</ApplicationBtn>
                         </FestInfo>
-                      <MoreIcon src={more_button} alt="더보기" />
+                      {/* <MoreIcon src={more_button} alt="더보기" /> */}
                      </FestLeft>
                      <StatusClosed>
-                        <p>수락</p>
+                        <span>수락</span>
                         <ReviewBtn onClick={()=>navigate(`/mypage/company/${fest.festivalId}/review`)}>리뷰쓰기</ReviewBtn>
                     </StatusClosed>
                      
@@ -98,19 +102,21 @@ const MPFestivalFavored = () => {
           ) : (
             <>
               <OngoingFest>
-                <p>진행 및 예정인 행사</p>
+                <Type>진행 및 예정인 행사</Type>
                 <FestCardList>
                   {fest_data.slice(0, 2).map((fest, index) => (
                     <FestCard key={index}>
                       <FestImage src={fest.image} alt="" />
                      <FestLeft>
                         <FestInfo>
+                            <RealInfo>
                             <FestName>{fest.festivalName}</FestName>
                             <p>{fest.location}</p>
                             <p>{fest.period}</p>
+                            </RealInfo>
                             <ApplicationBtn>지원서 보기</ApplicationBtn>
                         </FestInfo>
-                      <MoreIcon src={more_button} alt="업체더보기" />
+                      {/* <MoreIcon src={more_button} alt="업체더보기" /> */}
                      </FestLeft>
                      <Status>
                         <p>심사 중</p>
@@ -130,22 +136,24 @@ const MPFestivalFavored = () => {
               </OngoingFest>
 
               <ClosedFest>
-                <p>종료된 행사</p>
+                <Type>종료된 행사</Type>
                 <FestCardList>
                   {fest_data.slice(2, 4).map((fest, index) => (
                     <FestCard key={index}>
                       <FestImage src={fest.image} alt="" />
                      <FestLeft>
                         <FestInfo>
+                            <RealInfo>
                             <FestName>{fest.festivalName}</FestName>
                             <p>{fest.location}</p>
                             <p>{fest.period}</p>
+                            </RealInfo>
                             <ApplicationBtn>지원서 보기</ApplicationBtn>
                         </FestInfo>
-                      <MoreIcon src={more_button} alt="업체더보기" />
+                      {/* <MoreIcon src={more_button} alt="업체더보기" /> */}
                      </FestLeft>
                      <StatusClosed>
-                        <p>수락</p>
+                        <span>수락</span>
                         <ReviewBtn onClick={()=>navigate(`/mypage/company/${fest.festivalId}/review`)}>리뷰쓰기</ReviewBtn>
                     </StatusClosed>
                     </FestCard>
@@ -175,7 +183,10 @@ export default MPFestivalFavored;
 
 
 const PageWrapper = styled.div`
-  padding: 170px;
+  width: 100%;
+  max-width: 1200px;
+  margin: 130px auto;
+  box-sizing: border-box;
 `
 
 const Fixed = styled.div`
@@ -186,6 +197,13 @@ const Fixed = styled.div`
   /* background-color: white;  */
   z-index: 1000; /* 다른 요소보다 위 */
 `;
+
+const Type = styled.div`
+  font-size: 18px;
+  font-weight: 400;
+  margin: 0 8%;
+`
+
 
 
 const Title = styled.div`
@@ -200,7 +218,7 @@ const Back = styled.span`
   font-weight: bold;
   color: #555;
   text-decoration: underline;
-
+  margin: 0 8%;
   &:hover {
     color: #f97e6c;
   }
@@ -221,91 +239,124 @@ const FestCardList = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom: 40px;
-  gap: 20px;
+  margin-bottom: 20px;
+  gap: 60px;
 `;
 
 const FestCard = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 20px;
-  padding: 15px;
+  gap: 30px;
   width: 80%;
   max-width: 1000px;
-
-    border: 1px solid #ddd;
-    border-radius: 30px;
 `;
 
 const FestImage = styled.img`
-  width: 270px;
-  height: 360px;
-  border-radius: 20px;
+  width: 210px;
+  height: 280px;
+  object-fit: cover;
+  /* border-radius: 20px; */
   cursor: pointer;
 `;
 
 const FestLeft = styled.div`
-    display: flex;
-    flex: 1;
-    margin-left: 20px;
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
 `
 const Status = styled.div`
+    color: #6C53E8;
     
 `
 const StatusClosed = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    align-items: center;
     
+    span{
+        color: #708A62;
+    }
 `
 
 const ReviewBtn = styled.button`
-    width: 200px;
+    width: 130px;
     padding: 20px 0;
+    border-radius: 20px;
+    border-width: 1px;
+    border-color: rgba(0, 0, 0, 0.25);
+    background: #F4EDED;
+    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.25);
     cursor: pointer;
-`
+
+    &:hover{
+      background-color: #dcd7d7;
+    }
+`;
 
 const FestInfo = styled.div`
   p{
-    font-size: 18px;
+    font-size: 14px;
   }
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 150px;
   justify-content: center;
+  flex:1;
 `;
 
+const RealInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`
+
 const FestName = styled.span`
-  font-size: 25px;
+  font-size: 20px;
   font-weight: 700;
   cursor: pointer;
 `;
 
 const ApplicationBtn = styled.button`
   /* display: inline-block; */
-  margin-top: 100px;
-  padding: 8px 12px;
-  width: 150px;
-  height: 50px;
-  cursor: pointer;
+    width: 120px;
+    height: 40px;
+    padding: 20px 0;
+    border-radius: 20px;
+    border-width: 1px;
+    border-color: rgba(0, 0, 0, 0.25);
+    background: #F4EDED;
+    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
 `;
 
 
 
 const Content = styled.div`
-  border-top: 2px solid #ccc;
-  padding: 50px;
-  margin: -20px;
+  /* padding: 50px; */
+  /* margin: -20px; */
 `;
 
 
-const MoreIcon = styled.img`
-  width: 24px;
-  height: 24px;
-  cursor: pointer;
-`;
+// const MoreIcon = styled.img`
+//   width: 24px;
+//   height: 24px;
+//   cursor: pointer;
+// `;
 
 const More = styled.span`
+  margin-bottom: 80px;
+  margin-right: 100px;
+  margin-top: 20px;
   align-self: flex-end;
   cursor: pointer;
+  font-size: 14px;
   font-weight: bold;
   color: #555;
   text-decoration: underline;
